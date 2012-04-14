4fd1444f821b249a8229ca46c06c2d1364132e40
Vitor Pacheco <vitor-p.c@hotmail.com>
2012-04-14 00:04:26 -0300

    Migrate create_articles.

--- db/migrate/20120414024731_create_articles.rb
+++ db/migrate/20120414024731_create_articles.rb
@@ -1,8 +1,17 @@
 class CreateArticles < ActiveRecord::Migration
-  def change
+
+  def self.up
     create_table :articles do |t|
+      t.string :title
+      t.text  :body
+      t.datetime  :published_at
 
       t.timestamps
     end
   end
+
+  def self.down
+    drop_table :articles
+  end
+
 end
