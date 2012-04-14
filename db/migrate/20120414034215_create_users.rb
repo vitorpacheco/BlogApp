1a04dff094ca62e37cfb1dfaf223cb999274b0c5
Vitor Pacheco <vitor-p.c@hotmail.com>
2012-04-14 00:45:31 -0300

    Alteração na migração create_users.

--- db/migrate/20120414034215_create_users.rb
+++ db/migrate/20120414034215_create_users.rb
@@ -1,5 +1,6 @@
 class CreateUsers < ActiveRecord::Migration
-  def change
+
+  def self.up
     create_table :users do |t|
       t.string :email
       t.string :password
@@ -7,4 +8,9 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
   end
+
+  def self.down
+    drop_table :users
+  end
+
 end
