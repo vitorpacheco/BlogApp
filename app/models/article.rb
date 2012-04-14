c66e34db2b488d7040bd67e20fb259d255aafabc
Vitor Pacheco <vitor-p.c@hotmail.com>
2012-04-14 01:16:58 -0300

    Definindo o relacionamento entre os models User e Article.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,12 @@
 class Article < ActiveRecord::Base
 
   attr_accessible :body, :excerpt, :location, :published_at, :title
+
   validates :title, :presence => true
+
   validates :body, :presence => true
+
+  belongs_to :user
 
   def long_title
     "#{title} - #{published_at}"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+
   attr_accessible :email, :password
+
+  has_one :profile
+
 end
