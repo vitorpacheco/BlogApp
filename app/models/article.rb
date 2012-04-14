8f687f019e4222e5db4b01847ee0b35c461fdc65
Vitor Pacheco <vitor-p.c@hotmail.com>
2012-04-14 00:42:52 -0300

    Adicionado o método Article::long_title.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,11 @@
 class Article < ActiveRecord::Base
+
   attr_accessible :body, :excerpt, :location, :published_at, :title
+  validates :title, :presence => true
+  validates :body, :presence => true
+
+  def long_title
+    "#{title} - #{published_at}"
+  end
+
 end
